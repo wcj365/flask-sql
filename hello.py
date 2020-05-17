d621ab30672c09ba0551c41b07c97e1c21b97e0e
Jay Wang <wcj365@gmail.com>
2020-05-17 15:49:26 +0000

    .

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/test.db'
+#app.config['SERVER_NAME'] = '0.0.0.0:8080'
 
 db = SQLAlchemy(app)
 
