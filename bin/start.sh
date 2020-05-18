df4c53a6480e4d2d1ca517e94b5363fd4d7580bc
Jay Wang, D.Sc <wcj365@gmail.com>
2020-05-17 20:26:22 -0400

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,11 @@
 export FLASK_APP=../hello.py
+# for notebooks.ai
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=8080
+# for MITRE ECE lab
+#export FLASK_RUN_HOST=das10.mitre.org
+#export FLASK_RUN_PORT=5000
+# for pythonanywhere.com
+#export FLASK_RUN_HOST=das10.mitre.org
+#export FLASK_RUN_PORT=5000
 flask run
