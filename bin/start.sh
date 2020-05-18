aab36e029e198a9ba2d0e6aa82435c728cc799df
Jay Wang, D.Sc <wcj365@gmail.com>
2020-05-17 22:03:48 -0400

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,7 @@
 export FLASK_APP=../hello.py
 # for notebooks.ai
 export FLASK_RUN_HOST=0.0.0.0
+export FLASK_DEBUG=1
 export FLASK_RUN_PORT=8080
 # for MITRE ECE lab
 #export FLASK_RUN_HOST=das10.mitre.org
